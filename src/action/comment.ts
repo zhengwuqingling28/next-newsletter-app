fd1b61a93c9e14df8f31aae022ebaee4db7b1ea4
HiImLawtSimp1e <nqh1703k2@gmail.com>
2024-05-28 11:13:50 +0700

    Update Server Action Commit

--- src/action/comment.ts
+++ src/action/comment.ts
@@ -1,14 +1,27 @@
 "use server";
 
+interface CommentFormData {
+  email: string;
+  name: string;
+  comment: string;
+}
+
 import { revalidatePath } from "next/cache";
 
-export const addComment = async (formData) => {
-  const email = formData.get("email");
-  const name = formData.get("name");
-  const comment = formData.get("comment");
+export const addComment = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+  const name = formData.get("name") as string;
+  const comment = formData.get("comment") as string;
+
+  if (!email || !name || !comment) {
+    throw new Error("Form data is missing one or more fields");
+  }
+
+  const commentData: CommentFormData = { email, name, comment };
+
   const res = await fetch("http://localhost:3000/api/comments", {
     method: "POST",
-    body: JSON.stringify({ email, name, comment }),
+    body: JSON.stringify(commentData),
     headers: { "Content-Type": "application/json" },
   });
   const data = await res.json();
--- src/action/newsletter.ts
+++ src/action/newsletter.ts
@@ -1,9 +1,17 @@
 "use server";
-export const registerNewsletter = async (formData) => {
-  const email = formData.get("email");
+
+interface NewsletterFormData {
+  email: string;
+}
+
+export const registerNewsletter = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+
+  const newsletterData: NewsletterFormData = { email };
+
   const res = await fetch("http://localhost:3000/api/newsletter", {
     method: "POST",
-    body: JSON.stringify(email),
+    body: JSON.stringify(newsletterData),
     headers: { "Content-Type": "application/json" },
   });
   const data = await res.json();
